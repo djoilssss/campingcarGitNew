22fe5ac095d005a15713877cef331e541a467e48
yoojaee <jhyoo96@nexonstar.org>
2023-02-22 12:05:58 +0900

    no message

--- campingcar/src/main/java/kr/co/ccrent/service/FaqBoardService.java
+++ campingcar/src/main/java/kr/co/ccrent/service/FaqBoardService.java
@@ -9,18 +9,23 @@ import kr.co.ccrent.dto.Criteria;
 
 public interface FaqBoardService {
 
+	//글 추가
 	public void register(FaqBoardDTO boardDTO);
 	
+	//글 상세보기
 	public FaqBoardDTO read(int bno);
-	
+		
+	//글 수정하기
 	public void modify(FaqBoardDTO boardDTO);
 	
+	//글 지우기
 	public int remove(int bno);
 	
 	//public List<FaqBoardDTO> getList();
 	
+	//글 목록
 	public List<FaqBoardDTO> getAll(Criteria cri) throws Exception;
-	  
+	
 	public int getAll_Count(Criteria cri) throws Exception;
 	
 	public List<FaqBoardDTO> faq_get(Criteria cri)throws Exception;
